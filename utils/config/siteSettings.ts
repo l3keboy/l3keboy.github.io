763ef044bf2aadbddf22138ea983e236ab96abbc
Luke Hendriks <66973798+l3keboy@users.noreply.github.com>
2026-08-24 13:07:05 +0200

    Update siteSettings.ts

--- utils/config/siteSettings.ts
+++ utils/config/siteSettings.ts
@@ -1,13 +1,31 @@
 export type ISiteSettings = {
+  urls: {
+    githubUrl: string;
+    linkedInUrl: string;
+  }
+  siteMetadata: {
+    maintenance: boolean;
+    supportedSiteLanguages: string[]
+  }
+  currentEmployee: string
+  currentEducation: string
+  currentLocation: string
+  currentLanguages: string[]
   title: string;
-  linkedInUrl: string;
-  githubUrl: string;
-  maintenance: boolean;
 };
 
 export const siteSettings: ISiteSettings = {
+  urls: {
+    githubUrl: "https://github.com/l3keboy",
+    linkedInUrl: "https://www.linkedin.com/in/luke-h-421515225/",
+  },
+  siteMetadata: {
+    supportedSiteLanguages: ["en"],
+    maintenance: true,
+  },
+  currentEmployee: "CZ",
+  currentLocation: "NL",
+  currentEducation: "HBO-ICT - Fontys",
+  currentLanguages: ["nl", "en"],
   title: "Luke Hendriks",
-  linkedInUrl: "https://www.linkedin.com/in/luke-h-421515225/",
-  githubUrl: "https://github.com/l3keboy",
-  maintenance: true,
 };

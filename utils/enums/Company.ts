9b47875f899bb2dbcef4becb8c5da32ff1df32bb
Luke Hendriks <66973798+l3keboy@users.noreply.github.com>
2026-08-28 20:51:58 +0200

    Update Company.ts

--- utils/enums/Company.ts
+++ utils/enums/Company.ts
@@ -6,15 +6,3 @@ export enum Companies {
   MICROSOFT = "Microsoft",
   PEOPLECERT = "PeopleCert",
 }
-
-// TODO Company URLs
-// TODO Company images on certificationContainer
-
-export const CompanyUrl = {
-  [Companies.COMPTIA]: null,
-  [Companies.DASA]: null,
-  [Companies.EXIN]: null,
-  [Companies.KNOWLEDGE_PILLARS]: null,
-  [Companies.MICROSOFT]: null,
-  [Companies.PEOPLECERT]: null,
-};
